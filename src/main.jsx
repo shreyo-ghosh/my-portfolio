8a083f8c869eb5f873f03b4da2d0768f2c36577d
shreyo-ghosh <90380532+shreyo-ghosh@users.noreply.github.com>
2026-03-23 01:41:32 +0530

    Replace Home component with App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Home from "./Home";
+import App from "./App";
 import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<App />);
