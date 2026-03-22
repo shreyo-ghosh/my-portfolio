412e984fcd19bb4f03074cd2aa53dce54984ab3d
shreyo-ghosh <90380532+shreyo-ghosh@users.noreply.github.com>
2026-03-23 02:33:01 +0530

    Simplify App component to render only Home
    Removed routing and navigation components, simplifying the App to only render the Home component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
-import ChatBot from "./ChatBot";
-import BlogAgent from "./BlogAgent";
 
 function App() {
-  return (
-    <Router>
-      <div className="app">
-        <nav className="nav-menu">
-          <Link to="/" className="nav-link">Home</Link>
-          <Link to="/chatbot" className="nav-link">ChatBot</Link>
-          <Link to="/blog" className="nav-link">Blog Agent</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/chatbot" element={<ChatBot />} />
-          <Route path="/blog" element={<BlogAgent />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <Home />;
 }
 
 export default App;
