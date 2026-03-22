27e4b2924273b0403670097c702605a26babc164
shreyo-ghosh <90380532+shreyo-ghosh@users.noreply.github.com>
2026-03-23 02:24:32 +0530

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import ChatBot from "./ChatBot";
 import BlogAgent from "./BlogAgent";
@@ -11,13 +11,13 @@ function App() {
         <nav className="nav-menu">
           <Link to="/" className="nav-link">Home</Link>
           <Link to="/chatbot" className="nav-link">ChatBot</Link>
-          <Link to="/blog-agent" className="nav-link">Blog Agent</Link>
+          <Link to="/blog" className="nav-link">Blog Agent</Link>
         </nav>
-        
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/chatbot" element={<ChatBot />} />
-          <Route path="/blog-agent" element={<BlogAgent />} />
+          <Route path="/blog" element={<BlogAgent />} />
         </Routes>
       </div>
     </Router>
